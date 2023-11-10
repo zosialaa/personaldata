86a81b274b14b7559a9d7dceaa59b53857af4cb5
zosialaa <116967685+zosialaa@users.noreply.github.com>
2023-11-10 10:54:52 +0100

    Update test_github.py

--- tests/test_github.py
+++ tests/test_github.py
@@ -4,11 +4,11 @@ from personaldata.github_statistics import GithubStatistics
 
 class GithubStatisticsTest(unittest.TestCase):
     def test_construktor(self):
-        client = GithubStatistics("zosialaa")
-        self.assertTrue(client is not None)
+        client3 = GithubStatistics("zosialaa")
+        self.assertTrue(client3 is not None)
 
 
-    def test_data(self):
+    def test_data_github(self):
         api_client2 = GithubStatistics('zosialaa')
         api_client2.data_github()
         self.assertTrue(api_client2 is not None )
