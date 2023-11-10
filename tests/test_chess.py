e2b379a86ff1b0b840bd8fb98d2892193096d2ff
zosialaa <116967685+zosialaa@users.noreply.github.com>
2023-11-10 10:53:50 +0100

    Update test_chess.py

--- tests/test_chess.py
+++ tests/test_chess.py
@@ -6,3 +6,8 @@ class StatisticsTests(unittest.TestCase):
     def test_construktor(self):
         client = ChessStatistics("zosialaa")
         client.data_chess()
+        
+    def test_data_chess(self):
+        client1= ChessStatistics("zosialaa")
+        client1.data_chess()
+        self.assertTrue(len(client1.table_data) > 0)
