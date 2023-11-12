0559152910e5df05d956c98c8b3072c525d7a958
zosialaa <zofia.lapinska@gmail.com>
2023-11-12 23:06:54 +0100

    new files

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ from personaldata.chess_statistics import ChessStatistics
 from personaldata.github_statistics import GithubStatistics
 from personaldata.getwebsite import GetWebsiteMixin
 
-#client1 = GetWebsiteMixin()
-#html = client1.get_website_html("https://www.chess.com/pl/member/zosialaa")
-#print(html)
 
 if __name__ == '__main__':
-    
-        client = ChessStatistics("zosialaa")
-        client.data_chess()
-        
-        client1 = GithubStatistics("zosialaa")
-        client1.data_github()
+
+    client = ChessStatistics("zosialaa")
+    client.data_chess()
+
+    client1 = GithubStatistics("zosialaa")
+    client1.data_github()
