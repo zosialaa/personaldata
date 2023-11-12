b594df701f4f2d8863cc7e29f9c9b94bc138a2b6
zosialaa <zofia.lapinska@gmail.com>
2023-11-12 23:35:50 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'personaldata Python package'
+LONG_DESCRIPTION = 'Python package.'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="personaldata",
+    version=VERSION,
+    author="zosialaa",
+    author_email="zofia.lapinska@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=open("requirements.txt").readlines(),
+
+    keywords=['python', 'personaldata'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
