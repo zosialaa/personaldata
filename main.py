db05a7bab77a0306e4057001627243534618d2f7
zosialaa <116967685+zosialaa@users.noreply.github.com>
2023-11-12 23:20:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,10 @@ from personaldata.getwebsite import GetWebsiteMixin
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
+
 
     client = ChessStatistics("zosialaa")
     client.data_chess()
 
     client1 = GithubStatistics("zosialaa")
     client1.data_github()
-=======
-    
-        client = ChessStatistics("zosialaa")
-        client.data_chess()
-        
-        client1 = GithubStatistics("zosialaa")
-        client1.data_github()
->>>>>>> 5528da7c6495c5ca97e21a8a6eaf790559842679
